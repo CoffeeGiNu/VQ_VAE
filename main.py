c3a8c513dba517b9161d392661df66305e9186a7
CoffeeGiNu <co4800@protonmail.com>
2022-07-06 23:41:57 +0900

    c

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ parser.add_argument('-de', '--dim_embedding', default=64, type=int)
 parser.add_argument('-nes', '--num_embeddings', default=512, type=int)
 parser.add_argument('-cc', '--commitment_cost', default=0.25, type=float)
 parser.add_argument('-d', '--decay', default=0.99, type=float)
-parser.add_argument('-lr', '--learning_rate', default=1e-3, type=float)
+parser.add_argument('-lr', '--learning_rate', default=2e-3, type=float)
 
 args = parser.parse_args()
 
